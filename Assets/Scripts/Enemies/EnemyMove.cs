3e10d59e6ce6b28d53a58c4ad6362b70b875cbb9
Jhonny <jhmsouza10@gmail.com>
2021-09-14 21:21:52 -0300

    Add enemy move

---
+++ Assets/Scripts/Enemies/EnemyMove.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class EnemyMove : MonoBehaviour
+{
+    public Vector2 enemyDirection;
+    public bool isRight = false;
+    public float timeForTurn = 0f;
+    public Rigidbody2D myRigidbody2D;
+    public float enemySwipeDuration = .2f;
+
+    public void Update()
+    {
+        MoveEnemy();
+        Turn();
+        timeForTurn++;
+        if (timeForTurn > 500)
+        {
+            timeForTurn = 0;
+        }
+    }
+    public void MoveEnemy() 
+    {
+        if (!isRight)
+        {
+            transform.Translate(enemyDirection * Time.deltaTime);
+            myRigidbody2D.transform.DOScaleX(-1, enemySwipeDuration);
+
+        }
+        else
+        {
+            transform.Translate(-enemyDirection * Time.deltaTime);
+            myRigidbody2D.transform.DOScaleX(1, enemySwipeDuration);
+
+        }
+    }
+    public void Turn()
+    {
+        if(timeForTurn < 250)
+        {
+            isRight = true;            
+        }
+        else
+        {
+            isRight = false;
+        }
+    }
+}
