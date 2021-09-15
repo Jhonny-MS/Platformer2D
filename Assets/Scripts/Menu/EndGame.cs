0662d4fd4abe575cc763c1b8aa17fb2d36b7b114
Jhonny <jhmsouza10@gmail.com>
2021-09-14 22:17:48 -0300

    Add EndMenu

---
+++ Assets/Scripts/Menu/EndGame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    public GameObject uiEndGame;
+    public string tagToCheck = "Player";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(tagToCheck))
+        {
+            CallEndGame();
+        }
+    }
+    public void CallEndGame()
+    {
+        uiEndGame.SetActive(true);
+    }
+}
